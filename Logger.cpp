f1779358acfcaffddc612720880d7730c2a5294d
= <danielmonteiro@id.uff.br>
2016-10-12 07:53:46 -0300

    Disabling -for now- the logger in Android

--- Logger.cpp
+++ Logger.cpp
@@ -10,11 +10,7 @@
 namespace odb {
 	void Logger::log(std::string format, ...) {
 		#ifdef __ANDROID__
-		va_list args;
-		auto fmt = format.c_str();
-		va_start(args, fmt);
-		__android_log_print(ANDROID_LOG_INFO, "Logger::log", format.c_str(), args );
-		va_end(args);
+
 		#endif
 	}
 }

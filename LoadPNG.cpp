d1fc9c075b58db096e2b26abd71110239f095cee
TheFakeMontyOnTheRun <danielmonteiro@id.uff.br>
2017-05-31 23:35:20 +0100

    Perform some aproximations for DOS versions
    This improves the rendering quality somewhat.

--- LoadPNG.cpp
+++ LoadPNG.cpp
@@ -19,7 +19,6 @@ std::shared_ptr<odb::NativeBitmap> loadPNG(const std::string filename, std::shar
     int xSize;
     int ySize;
     int components;
-    
 #if defined(TARGET_IOS)
     stbi_convert_iphone_png_to_rgb(1);
 #endif
@@ -28,6 +27,28 @@ std::shared_ptr<odb::NativeBitmap> loadPNG(const std::string filename, std::shar
     auto rawData = new int[ xSize * ySize ];
     std::memcpy( rawData, image, xSize * ySize * 4 );
     stbi_image_free(image);
+
+#ifdef OSMESA
+    for ( int c = 0; c < xSize * ySize; ++c  ) {
+        int origin = rawData[ c ];
+
+        int r = (origin & 0xFF000000) >> 24;
+        int g = (origin & 0x00FF0000) >> 16;
+        int b = (origin & 0x0000FF00) >> 8;
+        int a = (origin & 0x000000FF);
+
+        int r20 = (( r / 20 ) * 20 );
+        int g20 = (( g / 20 ) * 20 );
+        int b20 = (( b / 20 ) * 20 );
+
+        int pixel = a;
+        pixel += ( r20 << 24 );
+        pixel += ( g20 << 16 );
+        pixel += ( b20 <<  8 );
+
+        rawData[ c ] = pixel;
+    }
+#endif
 
     return std::make_shared<odb::NativeBitmap>( filename, xSize, ySize, rawData );
 }
